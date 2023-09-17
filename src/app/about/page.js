551fa53b20bcbe7aac3aa7074aafc43459dc1749
hafsa abdirahman <hafsa@infra04-wg012.eduroam.wireless.umn.edu>
2023-09-17 17:44:49 -0500

    figured out routing with app folder

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Page() {
+	return ( 
+		<main>
+			<h1>about page</h1>
+			<h2>
+				<Link href="/">Home</Link>
+			</h2>
+		</main>
+	);
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -7,6 +8,9 @@ export default function Home() {
         <h1 className={styles.main}>
           CS-PARADIGMS
         </h1>
+	<h2>
+	  <Link href="/about">hello</Link>
+	</h2>
       </div>
     </main>
   )
